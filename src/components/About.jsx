2dc58949fa3e4a080fc359b851c9477012087a4a
joram <chogejoram@gmail.com>
2024-04-10 22:17:15 +0300

    add background color

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function About() {
   return (
-      <div>
+      <div style={{ backgroundColor: 'grey' }}>
         <h1>About Us</h1>
         <p>
           Welcome to our website! We are dedicated to providing high-quality products and excellent services to our customers. Our team is passionate about what we do, and we strive to exceed your expectations.
@@ -11,7 +11,7 @@ function About() {
           If you are in the mood to try something tasty then Caffeine Coffee shop is the place to be.
         </p>
         <p>
-         Do you love coffee? If your answer is yes come over and try some of ours it is the best. 
+          Do you love coffee? If your answer is yes come over and try some of ours it is the best. 
         </p>
       </div>
     );
