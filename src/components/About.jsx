cfb3b106626b5f1ef47986d7410e7474f81e4c7c
joram <chogejoram@gmail.com>
2024-04-10 22:07:52 +0300

    add word

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,21 @@
-import { Link } from "react-router-dom"
+import React from 'react';
 
-//App functional component
-const App = ()=>{
+function About() {
   return (
-      <h1>About Page</h1>
-  )
-}
+      <div>
+        <h1>About Us</h1>
+        <p>
+          Welcome to our website! We are dedicated to providing high-quality products and excellent services to our customers. Our team is passionate about what we do, and we strive to exceed your expectations.
+        </p>
+        <p>
+          If you are in the mood to try something tasty then Caffeine Coffee shop is the place to be.
+        </p>
+        <p>
+         Do you love coffee? If your answer is yes come over and try some of ours it is the best. 
+        </p>
+      </div>
+    );
+  }
+
 
 export default App
