1e478529108037c398c834d76387ababd791dd27
joram <chogejoram@gmail.com>
2024-04-15 23:37:56 +0300

    Add update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,23 +1,27 @@
 
 function About() {
   return (
-      <div style={{ backgroundColor: 'grey' }}>
+      <div style={{ backgroundColor: 'grey', backgroundImage: 'url(/src/Images.jpg)', backgroundSize: 'cover' }}>
 
-        <h1>About Us</h1>
-        <p>
+        <h1 style={{ fontSize: '2.5rem', textAlign: "center", fontWeight: 'bold' }}>About Us</h1>
+        <ul style={{ fontSize: '1.1rem', listStyleType: 'disc', paddingLeft: '1.5rem', lineHeight: '2rem' }}>
+        <li>
           Welcome to our website! We are dedicated to providing high-quality products and excellent services to our customers. Our team is passionate about what we do, and we strive to exceed your expectations.
-        </p>
-        <p>
+        </li>
+        <li>
           At caffeine, we're passionate about bringing you the finest coffees from around the world, carefully curated to delight your senses with every sip.
           From rich single-origin beans to artisanal blends and indulgent specialty drinks, there's something for every coffee lover to enjoy.
-        </p>
-        <p>
+          
+        </li>
+        <li>
           If you are in the mood to try something tasty then Caffeine Coffee shop is the place to be.
-        </p>
-        <p>
-          Do you love coffee? If your answer is yes come over and try some of ours it is the best. 
-        </p>
-        <p>
+          
+        </li>
+        <li>
+          <strong>Do you love coffee?</strong> If your answer is yes come over and try some of ours it is the best. 
+          
+        </li>
+        <li>
         We recognize the immense effort and dedication of coffee farmers who work tirelessly to cultivate the perfect beans. As such, 
         we are committed to ensuring that they receive fair compensation for their labor and expertise. By engaging in direct trade relationships with coffee farmers and cooperatives, 
         we bypass traditional intermediaries and guarantee that farmers receive a fair price for their produce. 
@@ -26,8 +30,9 @@ function About() {
         We actively engage in initiatives that promote environmentally sustainable farming practices, support social welfare programs in coffee-producing regions, and advocate for fair trade policies. 
         By prioritizing ethical business practices, 
         we strive to create a more equitable and sustainable coffee industry for generations to come.
-        </p>
-      
+        
+        </li>
+      </ul>
       </div>
     );
   }
